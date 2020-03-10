1c90055070b39d0a93abdb225416db9d522f9f84
Aditya Raj <aditya.x510@gmail.com>
2020-03-10 20:42:50 +0530

    Create loginSimpleTest.py

---
+++ seleniumTest/loginSimpleTest.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+import time
+
+driver = webdriver.Chrome('../Drivers/x32/chromedriver.exe')
+
+driver.get("https://gnsaddy.github.io/webAutomationSelenium/")
+loginEmail = driver.find_element_by_xpath('//*[@id="inputEmail"]')
+loginPass = driver.find_element_by_xpath('//*[@id="inputPassword"]')
+time.sleep(2)
+loginEmail.send_keys("aditya@gmail.com")
+loginPass.send_keys("testing")
+time.sleep(5)
+btn = driver.find_element_by_xpath('/html/body/div/div/div/div/div/form/input')
